76835d4e46f7c365ce4ecb7bf025cc0abb2386c4
hyx546 <love.1179612384@qq.com>
2020-04-08 15:12:17 +0800

    feat:React-redux的使用

--- react-imooc/src/Test.js
+++ react-imooc/src/Test.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import {addGUN,removeGUN,addGunAsync} from './index.redux'
 
 export class Test extends Component {
   render() {
-    const store = this.props.store
-    const addGUN  =this.props.addGUN
-    const removeGUN  =this.props.removeGUN
-    const addGunAsync = this.props.addGunAsync
-    const num = store.getState()
     return (
       <div>
-        <h1>现在有机枪{num}把</h1>
-        <button onClick={() => store.dispatch(addGUN())}>申请武器</button>
-        <button onClick={() => store.dispatch(removeGUN())}>上缴武器</button>
-        <button onClick={() => store.dispatch(addGunAsync())}>拖两天再给</button>
+        <h1>现在有机枪{this.props.num}把</h1>
+        <button onClick={ this.props.addGUN}>申请武器</button>
+        <button onClick={ this.props.removeGUN}>上缴武器</button>
+        <button onClick={ this.props.addGunAsync}>拖两天再给</button>
       </div>
     )
   }
 }
+const mapStatetoProps = (state) => {
+  return {
+    num:state
+  }
+}
+const actionCreators = {addGUN,removeGUN,addGunAsync}
 
+Test = connect(mapStatetoProps,actionCreators)(Test)
 export default Test
--- react-imooc/src/index.js
+++ react-imooc/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Test from './Test'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
-import { counter ,addGUN,removeGUN,addGunAsync} from './index.redux'
+import {Provider} from 'react-redux'
+import { counter } from './index.redux'
 
-const store = createStore(counter,applyMiddleware(thunk))
 
-function render() {
-  ReactDom.render(<Test store={store} addGUN={addGUN} removeGUN={removeGUN} addGunAsync={addGunAsync}/>,document.getElementById('root'))
-}
-render()
 
-store.subscribe(render)
+// const store = createStore(counter,applyMiddleware(thunk))
+const store = createStore(counter,compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension?window.devToolsExtension(): f=>f
+))
+
+// function render() {
+//   ReactDom.render(<Test store={store} addGUN={addGUN} removeGUN={removeGUN} addGunAsync={addGunAsync}/>,document.getElementById('root'))
+// }
+// render()
+
+// store.subscribe(render)
+
+ReactDom.render(
+  (<Provider store={store}>
+    <Test/>,
+    </Provider>),
+  document.getElementById('root'))
