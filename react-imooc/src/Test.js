37f815bc6d357de770cd0a690f312661091a494c
hyx546 <love.1179612384@qq.com>
2020-04-08 14:40:53 +0800

    feat:Redux-thunk的使用

--- react-imooc/src/Test.js
+++ react-imooc/src/Test.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import {addGUN} from './index.redux'
 
 export class Test extends Component {
   render() {
     const store = this.props.store
+    const addGUN  =this.props.addGUN
+    const removeGUN  =this.props.removeGUN
+    const addGunAsync = this.props.addGunAsync
     const num = store.getState()
     return (
       <div>
         <h1>现在有机枪{num}把</h1>
         <button onClick={() => store.dispatch(addGUN())}>申请武器</button>
+        <button onClick={() => store.dispatch(removeGUN())}>上缴武器</button>
+        <button onClick={() => store.dispatch(addGunAsync())}>拖两天再给</button>
       </div>
     )
   }
--- react-imooc/src/index.js
+++ react-imooc/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Test from './Test'
-import {createStore} from 'redux'
-import { counter } from './index.redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { counter ,addGUN,removeGUN,addGunAsync} from './index.redux'
 
-const store = createStore(counter)
+const store = createStore(counter,applyMiddleware(thunk))
 
 function render() {
-  ReactDom.render(<Test store={store}/>,document.getElementById('root'))
+  ReactDom.render(<Test store={store} addGUN={addGUN} removeGUN={removeGUN} addGunAsync={addGunAsync}/>,document.getElementById('root'))
 }
 render()
 
