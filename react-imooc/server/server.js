78a80f5d433ec302fc74176697ed054bb2ac2d49
hyx546 <love.1179612384@qq.com>
2020-04-10 16:49:22 +0800

    注册跳转+密码加密实现cmd5

--- react-imooc/server/server.js
+++ react-imooc/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const utils = require('utility')
 
 const userRouter = require('./user')
 
