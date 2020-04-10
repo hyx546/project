0365aee65390fb2ee3fb9184d7848d5eb163201d
hyx546 <love.1179612384@qq.com>
2020-04-10 15:52:00 +0800

    登录注册，express注册功能实现

--- react-imooc/server/server.js
+++ react-imooc/server/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 const userRouter = require('./user')
 
 const app = express()
+app.use(cookieParser()) //用来解析cookie
+app.use(bodyParser.json()) //用来解析post返回的json
+
 app.use('/user',userRouter)
 
 app.listen(9093,() => {
