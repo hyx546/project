65e0ae518fd4ea98aaa1f4feed7b18d0a2d28aad
hyx546 <love.1179612384@qq.com>
2020-04-17 16:49:31 +0800

    feat:简单demo的认识nextjs

---
+++ react/next/nextjs-Github/.next/static/webpack/static/development/pages/index.js.f88307a3dbec9eb93dae.hot-update.js
@@ -0,0 +1,34 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/xiaokeai/Documents/github/project/react/nextjs-Github/pages/index.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+// import React from 'react';    next已经包含了
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("span", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3,
+      columnNumber: 22
+    }
+  }, "hyx");
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.f88307a3dbec9eb93dae.hot-update.js.map
---
+++ react/next/nextjs-Github/pages/index.js
@@ -0,0 +1,4 @@
+// import React from 'react';    next已经包含了
+
+export default () => <span>hyx</span>
+
